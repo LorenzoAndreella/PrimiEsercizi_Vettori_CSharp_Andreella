c14158587c57bc7f92c62df485e5d42c01922354
LorenzoAndreella <14529876@itisrossi.vi.it>
2026-02-11 17:32:11 +0100

    Aggiunta es1, es2

---
+++ esercizio1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static System.Console;
+
+namespace Es1Vettore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool x;
+            int n;
+            do
+            {
+               WriteLine("Inserisci quanti elementi vuoi inserire nel vettore"); 
+               x = int.TryParse(ReadLine(), out n);
+                if (!x)
+                {
+                    WriteLine("Input non valido, inserisci un numero intero.");
+                }
+            } while (!x);
+
+            int[] vett = new int[n];                //creazione del vettore con dimensione n
+
+            for (int i = 0; i < vett.Length; i++)
+            {
+                do
+                {
+                    WriteLine("Inserisci un numero");
+                    x = int.TryParse(ReadLine(), out vett[i]);
+                    if (!x)
+                    {
+                        WriteLine("Input non valido, inserisci un numero intero.");
+                    } 
+                } while (!x);
+            }
+
+            int valido = 1;         //variabile che indica se il vettore è valido (1) o non valido (0)
+            for (int i=0; i < vett.Length; i++)
+            {
+                for (int j = i+1; j < vett.Length; j++)
+                {
+                  if (vett[i] == vett[j])               //se si trova un elemento uguale a un altro, il vettore non è valido
+                    {
+                        valido = 0;
+                        break;
+                    }
+                }
+            }
+
+            WriteLine(valido);          //stampa 1 se il vettore è valido, 0 se non è valido
+        }
+    }
+}
---
+++ esercizio2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+
+namespace Es2Vettore
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vett = new int[30];               //vettore di 30 elementi
+            Random rand = new Random();             
+            for (int i = 0; i < vett.Length; i++)
+            {
+                vett[i] = rand.Next(0, 101);            //numeri random da 0 a 100 (inclusi)
+                Write(vett[i] + " ");
+            }
+            WriteLine();
+            bool trovato = false;                   //variabile che indica se è stata trovata almeno una coppia di numeri tali che il primo sia il doppio dell'altro
+            
+            for (int i = 0; i < vett.Length; i++)       //ricerca coppie di numeri tali che il primo sia il doppio dell'altro
+            {
+                if (vett[i] % 2 == 0 && vett[i] != 0)               // può essere doppio solo se è pari e non nullo
+                {
+                    for (int j = 0; j < vett.Length; j++)
+                    {
+                        if (i != j && vett[i] == vett[j] * 2)               //se si trova una coppia, si stampa e si esce dal ciclo
+                        {
+                            Write($" / {vett[i]} {vett[j]}");
+                            trovato = true;                     //trovata la coppia e si esce dal ciclo
+                            break;                                      
+                        }
+                        
+                    }
+                }
+            }
+            if (!trovato)              //se non è stata trovata nessuna coppia, si stampa un messaggio
+            {
+                WriteLine("Non sono state trovate coppie di numeri tali che il primo sia il doppio dell'altro.");
+            }
+        }
+    }
+}
